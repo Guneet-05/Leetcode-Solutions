57eb625c2036cf1bc6889ec0636be54f0368b842
Guneet Malhotra <77784585+Guneet-05@users.noreply.github.com>
2022-07-01 07:34:44 +0530

    Time: 13 ms (50.13%), Space: 51.1 MB (39.17%) - LeetHub

--- 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.java
+++ 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.java
@@ -1,42 +1,22 @@
 class Solution {
-    
-    static class Pair implements Comparable<Pair>{
-        int noOfBoxes;
-        int noOfItemsPerUnitBox;
-        
-        Pair(int noOfBoxes,int noOfItemsPerUnitBox) {
-            this.noOfBoxes = noOfBoxes;
-            this.noOfItemsPerUnitBox = noOfItemsPerUnitBox;
-        }
-        
-        public int compareTo(Pair other) {
-            return other.noOfItemsPerUnitBox - this.noOfItemsPerUnitBox;
-        }
-    }
-    
     public int maximumUnits(int[][] boxTypes, int truckSize) {
+        Arrays.sort(boxTypes, (a,b) -> {return b[1] - a[1];});
+        
         int n = boxTypes.length;
-        Pair[] arr = new Pair[n];
+        int res = 0;
         
         for(int i=0;i<n;i++) {
-            arr[i] = new Pair(boxTypes[i][0],boxTypes[i][1]);
-        }
-        
-        Arrays.sort(arr);
-        
-        int res = 0;
-        for(int i=0;i<arr.length;i++) {
             if(truckSize == 0) {
                 break;
             } else {
-                if(arr[i].noOfBoxes > truckSize) {
-                    res += arr[i].noOfItemsPerUnitBox * (truckSize);
+                if(boxTypes[i][0] > truckSize) {
+                    res += boxTypes[i][1] * truckSize;
                     truckSize = 0;
                 } else {
-                    res += arr[i].noOfItemsPerUnitBox * (arr[i].noOfBoxes);
-                    truckSize -= arr[i].noOfBoxes;
+                    res += boxTypes[i][1] * boxTypes[i][0];
+                    truckSize -= boxTypes[i][0];
                 }
-            }   
+            }
         }
         
         return res;
