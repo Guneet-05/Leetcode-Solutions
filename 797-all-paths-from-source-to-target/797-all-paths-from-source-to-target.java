eef1769493f3cd8805ec4b7d61f7c833bec26c57
Guneet Malhotra <77784585+Guneet-05@users.noreply.github.com>
2022-07-05 09:00:21 +0530

    Time: 1 ms (99.86%), Space: 44.3 MB (86.83%) - LeetHub

--- 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.java
+++ 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.java
@@ -1,31 +1,35 @@
 class Solution {
+    List<List<Integer>> res;
     
-    public void DFS(int src,int dest,int[][] graph,boolean[] vis,List<Integer> psf,List<List<Integer>> res) {
-       
+    public void DFS(int src,int dest,int[][] graph,boolean[] vis,List<Integer> psf) {
+        
         if(vis[src] == true) return;
+        
+        if(src == dest) {
+            psf.add(dest);
+            List<Integer> copy = new ArrayList<>(psf);
+            res.add(copy);
+            psf.remove(psf.size() -1);
+            return;
+        }
         
         vis[src] = true;
         psf.add(src);
         
-        if(src == dest) {
-            res.add(new ArrayList<>(psf));
-        } 
-        
         for(int nbr : graph[src]) {
-            DFS(nbr,dest,graph,vis,psf,res);
+            DFS(nbr,dest,graph,vis,psf);
         }
         
-        
         vis[src] = false;
-        psf.remove(psf.size() - 1);
+        psf.remove(psf.size() -1);
     }
     
     public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
         List<Integer> psf = new ArrayList<>();
-        List<List<Integer>> res = new ArrayList<>();
+        res = new ArrayList<>();
         boolean[] vis = new boolean[graph.length];
         
-        DFS(0,graph.length-1,graph,vis,psf,res);
+        DFS(0,graph.length-1,graph,vis,psf);
         return res;
     }
 }
