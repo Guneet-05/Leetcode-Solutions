de8e905323f60d853834f951dc6a3c944c0bd58a
Guneet Malhotra <77784585+Guneet-05@users.noreply.github.com>
2022-07-09 11:29:51 +0530

    Time: 0 ms (100.00%), Space: 41.9 MB (89.77%) - LeetHub

---
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean searchMatrix(int[][] arr, int target) {
+        int i = 0;
+        int j = arr[0].length -1;
+        while(i < arr.length && j >= 0) {
+            if(arr[i][j] < target) {
+                i++;
+            } else if(arr[i][j] == target) {
+                return true;
+            }  else {
+                while(j >= 0) {
+                    if(arr[i][j] > target) {
+                        j--;
+                    } else if(arr[i][j] < target) {
+                        return false;
+                    } else {
+                        return true; 
+                    }
+                }
+            }
+        }
+        return false;
+    }
+}
