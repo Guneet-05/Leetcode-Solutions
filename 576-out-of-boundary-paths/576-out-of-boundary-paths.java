607d9e5a37eaf88487c40a2233d8d3b66d6f66e9
Guneet Malhotra <77784585+Guneet-05@users.noreply.github.com>
2022-07-16 09:01:04 +0530

    Time: 7 ms (75.21%), Space: 43.7 MB (19.38%) - LeetHub

---
+++ 576-out-of-boundary-paths/576-out-of-boundary-paths.java
@@ -0,0 +1,28 @@
+class Solution {
+    int m = 1000000007;
+    public long solve(int sr,int sc, int m, int n,int curr, int max,Long[][][] dp) {
+        
+        if((sr < 0 || sc < 0 || sr >= m || sc >= n)) return 1L;
+        
+        if(curr == max) {
+            return 0L;
+        }
+        
+        if(dp[sr][sc][curr] != null) return dp[sr][sc][curr];
+        
+        long ans1 = solve(sr,sc + 1,m,n,curr + 1,max,dp);
+        long ans2 = solve(sr + 1,sc,m,n,curr + 1,max,dp);
+        long ans3 = solve(sr,sc - 1,m,n,curr + 1,max,dp);
+        long ans4 = solve(sr - 1,sc,m,n,curr + 1,max,dp);
+        
+        return dp[sr][sc][curr] =  (ans1 + ans2 + ans3 + ans4) % 1000000007;
+    }
+    
+    public int findPaths(int m, int n, int maxMove, int startRow, int startColumn) {
+        
+        Long[][][] dp = new Long[m + 1][n + 1][maxMove + 1];
+        
+        
+       return (int)solve(startRow,startColumn,m,n,0,maxMove,dp);
+    }
+}
