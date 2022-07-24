3c12bd0c7b30a927425d0b3f4e2110cc9c5bf20b
Guneet Malhotra <77784585+Guneet-05@users.noreply.github.com>
2022-07-24 19:55:08 +0530

    Time: 1 ms (98.69%), Space: 42.9 MB (14.78%) - LeetHub

---
+++ 91-decode-ways/91-decode-ways.java
@@ -0,0 +1,37 @@
+class Solution {
+    
+    public int memo(String s,int idx,int[] dp) {
+        
+        if(idx == s.length()) return 1;
+        
+        if(dp[idx] != -1) return dp[idx];
+        
+        int ans1 = 0, ans2 = 0;
+        
+        if(s.charAt(idx) != '0') {
+            ans1 = memo(s,idx + 1,dp);
+        }
+        
+        if(idx + 1 < s.length() && s.charAt(idx) != '0') {
+            int code = (s.charAt(idx) - '0') * 10 + (s.charAt(idx + 1) - '0');
+            if(code >= 10 && code <= 26) {
+                ans2 = memo(s,idx + 2,dp);
+            }
+        }
+        
+        return dp[idx] = ans1 + ans2;
+    }
+    
+    public int numDecodings(String s) {
+        if(s.length() == 0 || s.equals("")) return 0;
+        
+        int[] dp = new int[s.length() + 1];
+        Arrays.fill(dp,-1);
+        
+        int ans = memo(s,0,dp);
+        
+        if(ans == -1) return 0;
+        
+        return ans;
+    }
+}
