745f0bd605f62172cb695d376ee494c72ba839e8
Guneet Malhotra <77784585+Guneet-05@users.noreply.github.com>
2022-08-06 11:57:52 +0530

    Time: 12 ms (36.33%), Space: 47.5 MB (28.41%) - LeetHub

---
+++ 264-ugly-number-ii/264-ugly-number-ii.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int nthUglyNumber(int n) {
+        if(n == 1) return 1;
+        
+        int ptr2 = 0, ptr3 = 0, ptr5 = 0;
+        
+        ArrayList<Integer> ugly = new ArrayList<>();
+        ugly.add(1);
+        
+        for(int i=1;i<n;i++) {
+            int a = ugly.get(ptr2) * 2;
+            int b = ugly.get(ptr3) * 3;
+            int c = ugly.get(ptr5) * 5;
+            
+            int min = Math.min(a,Math.min(b,c));
+            ugly.add(min);
+            
+            if(min == a) ptr2++;
+            if(min == b) ptr3++;
+            if(min == c) ptr5++;
+        }
+        
+        return ugly.get(n-1);
+    }
+}
