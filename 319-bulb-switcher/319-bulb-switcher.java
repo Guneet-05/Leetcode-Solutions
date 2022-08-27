29e4204a9b26cbd9718de8ce54e2709f725fbffa
Guneet Malhotra <77784585+Guneet-05@users.noreply.github.com>
2022-08-27 11:41:07 +0530

    Time: 1511 ms (5.12%), Space: 39.4 MB (87.43%) - LeetHub

--- 319-bulb-switcher/319-bulb-switcher.java
+++ 319-bulb-switcher/319-bulb-switcher.java
@@ -1,9 +1,9 @@
 class Solution {
     public int bulbSwitch(int n) {
-        
         int count = 0;
+        
         for(int i=1;i<=n;i++) {
-            int sqrt = (int)(Math.sqrt(i));
+            int sqrt = (int)Math.sqrt(i);
             if(sqrt * sqrt == i) count++;
         }
         
