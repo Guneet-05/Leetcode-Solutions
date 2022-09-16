bedb67ebe89d82aa0a75ca113762c4c0bca246f3
Guneet Malhotra <77784585+Guneet-05@users.noreply.github.com>
2022-09-16 18:27:49 +0530

    Time: 1 ms (39.02%), Space: 42.7 MB (52.08%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.java
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.java
@@ -1,24 +1,27 @@
 class Solution {
-    public boolean searchMatrix(int[][] arr, int target) {
-        int i = 0;
-        int j = arr[0].length -1;
-        while(i < arr.length && j >= 0) {
-            if(arr[i][j] < target) {
-                i++;
-            } else if(arr[i][j] == target) {
-                return true;
-            }  else {
-                while(j >= 0) {
-                    if(arr[i][j] > target) {
-                        j--;
-                    } else if(arr[i][j] < target) {
-                        return false;
-                    } else {
-                        return true; 
-                    }
-                }
-            }
+    
+    public boolean binSearch(int[][] matrix, int target, int i) {
+        int lo = 0;
+        int hi = matrix[0].length -1;
+        
+        while(lo <= hi) {
+            int mid = (lo + hi)/2;
+            
+            if(matrix[i][mid] == target) return true;
+            else if(matrix[i][mid] < target) lo = mid + 1;
+            else hi = mid -1;
         }
+        
+        return false;
+    }
+    
+    public boolean searchMatrix(int[][] matrix, int target) {
+        
+        for(int i=0;i<matrix.length;i++) {
+            boolean val = binSearch(matrix,target,i);
+            if(val == true) return true;
+        }
+        
         return false;
     }
 }
