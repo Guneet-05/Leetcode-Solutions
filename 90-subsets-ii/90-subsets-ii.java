426a028ceade30aa5321dc69e8165149fed406ff
Guneet Malhotra <77784585+Guneet-05@users.noreply.github.com>
2022-10-01 18:15:46 +0530

    Time: 2 ms (87.50%), Space: 44.4 MB (31.69%) - LeetHub

---
+++ 90-subsets-ii/90-subsets-ii.java
@@ -0,0 +1,32 @@
+class Solution {
+    
+    List<List<Integer>> ans;
+    
+    public void subsets(int[] nums, int idx,List<Integer> curr,boolean prev) {
+        
+        if(idx == nums.length) {
+            List<Integer> copy = new ArrayList<>(curr);
+            ans.add(copy);
+            return;
+        }
+        
+        //no
+        subsets(nums,idx+1,curr,false);
+        
+        //yes
+        
+        if(idx > 0 && nums[idx] == nums[idx-1] && prev == false)
+            return;
+        
+        curr.add(nums[idx]);
+        subsets(nums,idx + 1,curr,true);
+        curr.remove(curr.size()-1);
+    }
+    
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        Arrays.sort(nums);
+        ans = new ArrayList<>();
+        subsets(nums,0,new ArrayList<>(),true);
+        return ans;
+    }
+}
