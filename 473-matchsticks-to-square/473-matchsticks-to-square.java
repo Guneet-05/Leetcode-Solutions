0ed06eb9a6a3e282bdd02a2ea50d9646e6face84
Guneet Malhotra <77784585+Guneet-05@users.noreply.github.com>
2022-10-05 23:24:17 +0530

    Time: 216 ms (51.04%), Space: 41.8 MB (57.87%) - LeetHub

--- 473-matchsticks-to-square/473-matchsticks-to-square.java
+++ 473-matchsticks-to-square/473-matchsticks-to-square.java
@@ -1,8 +1,26 @@
 class Solution {
     
+    public boolean dfs(int[] matchsticks,int idx,int[] sides,int sideLength) {
+        //base case
+        if(idx == matchsticks.length) return true;
+        
+        for(int i=0;i<4;i++) {
+            //pruning
+            if(sides[i] + matchsticks[idx] <= sideLength) {
+                //place the idxth matchstick on ith side
+                sides[i] += matchsticks[idx];
+                boolean canWePlaceRemSticks = dfs(matchsticks,idx + 1,sides,sideLength);
+                if(canWePlaceRemSticks == true) return true;
+                sides[i] -= matchsticks[idx];
+            }
+        }
+        
+        return false;
+    }
+    
     public void reverse(int[] arr) {
         int lo = 0;
-        int hi = arr.length-1;
+        int hi = arr.length -1;
         
         while(lo < hi) {
             int temp = arr[lo];
@@ -11,38 +29,23 @@ class Solution {
             lo++;
             hi--;
         }
-        
-    }
-    
-    public boolean dfs(int[] matchsticks,int[] sides,int idx,int sideLength) {
-        
-        if(idx == matchsticks.length) return true;
-        
-        for(int i=0;i<4;i++) {
-            if(sides[i] + matchsticks[idx] <= sideLength) {
-                sides[i] += matchsticks[idx];
-                if(dfs(matchsticks,sides,idx + 1,sideLength) == true)
-                    return true;
-                sides[i] -= matchsticks[idx];
-            }
-        }
-        
-        return false;
     }
     
     public boolean makesquare(int[] matchsticks) {
-        if(matchsticks.length == 0 || matchsticks == null) return false;
+        if(matchsticks == null || matchsticks.length == 0) return false;
         
-        int p = 0;
+        int peri = 0;
+        for(int val : matchsticks) peri += val;
         
-        for(int val : matchsticks) p += val;
-        if(p % 4 != 0) return false;
+        if(peri % 4 != 0) return false;
         
-        int sideLength = p/4;
+        int sideLength = peri / 4;
         int[] sides = new int[4];
+        
+        //sort in descending order
         Arrays.sort(matchsticks);
         reverse(matchsticks);
         
-        return dfs(matchsticks,sides,0,sideLength);
+        return dfs(matchsticks,0,sides,sideLength);
     }
 }
